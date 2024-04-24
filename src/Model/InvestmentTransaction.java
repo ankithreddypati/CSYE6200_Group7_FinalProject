eca145a1bf622c00ab2d686ee05ece69db9203d6
Bhuvana-Ravikumar <114808384+Bhuvana-Ravikumar@users.noreply.github.com>
2024-04-24 18:30:25 -0400

    investmentmodel2

--- src/Model/InvestmentTransaction.java
+++ src/Model/InvestmentTransaction.java
@@ -18,5 +18,35 @@ public class InvestmentTransaction {
         this.description = description;
     }
 
-    
+    // Getters and Setters for all attributes
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public double getAmountInvested() {
+        return amountInvested;
+    }
+
+    public void setAmountInvested(double amountInvested) {
+        this.amountInvested = amountInvested;
+    }
+
+    public double getCurrentValue() {
+        return currentValue;
+    }
+
+   
 }
